7d4d4e41fd4f2475f86f41ce22dca2a19534ebd0
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-11-15 21:20:26 +0100

    leared how tipescript regognice if

--- TypeScriptTraining/src/app/main-content/main-content.component.ts
+++ TypeScriptTraining/src/app/main-content/main-content.component.ts
@@ -13,7 +13,7 @@ export class MainContentComponent {
   fruitList = ['Bannana'];
 
   constructor() {
-    this.fruit = this.fruitList.pop(); // wird als fehler gezeigt, da pop auch als undefined steht , da array auch leer sein könnte
+    // this.fruit = this.fruitList.pop(); // wird als fehler gezeigt, da pop auch als undefined steht , da array auch leer sein könnte
     
     let banana = this.fruitList.pop()   // hier kein fehler , da man durch die if abfrage mit undefined nicht zum pop kommt!
     if (banana != undefined) {
